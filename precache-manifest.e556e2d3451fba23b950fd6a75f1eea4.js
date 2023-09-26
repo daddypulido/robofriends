e3e306c67407e06c57c750d9e2d35b3d8cdb3eb4
daddypulido <Alexanderpulido10@gmail.com>
2023-09-26 06:51:50 -0700

    Updates

---
+++ precache-manifest.e556e2d3451fba23b950fd6a75f1eea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a4b58138d66d9cd08bc",
+    "url": "/robofriends/static/css/main.79fe2c7d.chunk.css"
+  },
+  {
+    "revision": "8a4b58138d66d9cd08bc",
+    "url": "/robofriends/static/js/main.af8ca7df.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "6743cfc3556d010d155d",
+    "url": "/robofriends/static/css/2.3ba1355b.chunk.css"
+  },
+  {
+    "revision": "6743cfc3556d010d155d",
+    "url": "/robofriends/static/js/2.0ef313da.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "71a58812e140af229ca3b0e1963cdca8",
+    "url": "/robofriends/index.html"
+  }
+];
